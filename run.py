c1e7bc766b5689efbb4f40c6eff8728c3fd90d02
Prasanna-Ganesh <120724118+Prasanna-Ganesh@users.noreply.github.com>
2023-05-22 15:48:31 +0530

    Update run.py

--- run.py
+++ run.py
@@ -4,3 +4,7 @@ from src.app import app
 if __name__ == '__main__':
     with app.app_context():
         app.run(debug=True)
+        
+        
+        
+# inside src/app i had written the code fon the contact book
